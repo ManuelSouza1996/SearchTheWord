dd770322fc87f8938d61cd894075d82e6ac64f75
burkerking72 <ryburke15@gmail.com>
2019-12-04 18:06:38 -0800

    Added a container class so that data can be passed internally when there is a level change.

--- app/src/main/java/com/ryanthomasburke/www/searchtheword/DifficultyActivity.java
+++ app/src/main/java/com/ryanthomasburke/www/searchtheword/DifficultyActivity.java
@@ -6,14 +6,23 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ryanthomasburke.www.searchtheword.Utility.Container;
+
 public class DifficultyActivity extends AppCompatActivity {
+
+    public static Container container;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulty);
+        container = new Container();
     }
 
-    public void goToGame(View view){
+    public void easyButton(View view){
+        container.setDifficulty(1);
+        container.setTotalScore(0);
+        container.setCurrentLevel(1);
         Intent intent = new Intent(this, GameActivity.class);
         this.startActivity(intent);
     }
